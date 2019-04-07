6671b565de2aa3044d97cf4675c72ae64889633b
Admin <nguyenduc049@gmail.com>
2019-04-07 23:00:18 +0700

    add

--- app/src/main/java/com/example/loginsocialnetwork/MainActivity.kt
+++ app/src/main/java/com/example/loginsocialnetwork/MainActivity.kt
@@ -2,11 +2,62 @@ package com.example.loginsocialnetwork
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.CallbackManager
+import kotlinx.android.synthetic.main.activity_main.*
+import com.facebook.FacebookException
+import com.facebook.AccessToken
+import com.facebook.login.LoginResult
+import com.facebook.FacebookCallback
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import android.content.Intent
+
+
+
+
+
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    var callbackManager: CallbackManager? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loginButton.setReadPermissions("email", "public_profile")
+
+        loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
+            override fun onSuccess(loginResult: LoginResult) {
+                // Lấy access token sử dụng LoginResult
+                val accessToken = loginResult.accessToken
+
+            }
+
+            override fun onCancel() {
+            }
+
+            override fun onError(exception: FacebookException) {
+            }
+        })
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+         val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+
+
+
     }
+
 }
