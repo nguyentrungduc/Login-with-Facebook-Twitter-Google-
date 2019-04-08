bacd8d947547701c2a13ce06df84a63ca2a68a64
ntduc <ntduc@framgia0378-pc.framgia.com>
2019-04-08 14:44:21 +0700

    add

--- app/src/main/java/com/example/loginsocialnetwork/MainActivity.kt
+++ app/src/main/java/com/example/loginsocialnetwork/MainActivity.kt
@@ -2,41 +2,43 @@ package com.example.loginsocialnetwork
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.facebook.CallbackManager
 import kotlinx.android.synthetic.main.activity_main.*
-import com.facebook.FacebookException
-import com.facebook.AccessToken
 import com.facebook.login.LoginResult
-import com.facebook.FacebookCallback
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import android.content.Intent
-
-
-
-
-
-
-
-
+import android.util.Log
+import com.facebook.*
+import com.facebook.appevents.AppEventsLogger
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import android.content.pm.PackageManager
+import android.util.Base64
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 
 class MainActivity : AppCompatActivity() {
 
-    var callbackManager: CallbackManager? = null
+    private var callbackManager: CallbackManager? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        FacebookSdk.sdkInitialize(applicationContext)
+        AppEventsLogger.activateApp(this)
+        getHashKey()
+
+        callbackManager = CallbackManager.Factory.create()
 
         loginButton.setReadPermissions("email", "public_profile")
 
         loginButton.registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(loginResult: LoginResult) {
-                // Lấy access token sử dụng LoginResult
                 val accessToken = loginResult.accessToken
+                Log.d("Main", accessToken.token)
 
             }
 
@@ -54,10 +56,90 @@ class MainActivity : AppCompatActivity() {
          val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
 
         val account = GoogleSignIn.getLastSignedInAccount(this)
+        Log.d("main", account.toString())
+
+        sign_in_button.setOnClickListener {
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 10)
+
+        }
+
+        getInfo()
+
+        sign_in_button.setOnClickListener {
+            mGoogleSignInClient.signOut()
+                .addOnCompleteListener(this) {
+                    Log.d("main", it.toString())
+                }
 
 
+        }
+
+        revoke_btn.setOnClickListener {
+            mGoogleSignInClient.revokeAccess()
+                .addOnCompleteListener(this) {
+                    Log.d("main", it.toString())
+                }
 
 
+        }
+
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 10) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    private fun handleSignInResult(task: Task<GoogleSignInAccount>?) {
+        try {
+            val account = task?.getResult(ApiException::class.java)
+
+        } catch (e: ApiException) {
+            Log.w("main", "signInResult:failed code=" + e.statusCode)
+        }
+
+
+    }
+
+    private fun getInfo() {
+        val acct = GoogleSignIn.getLastSignedInAccount(this)
+        if (acct != null) {
+            val personName = acct.displayName
+            val personGivenName = acct.givenName
+            val personFamilyName = acct.familyName
+            val personEmail = acct.email
+            val personId = acct.id
+            val personPhoto = acct.photoUrl
+            Log.d("main", personName)
+            Log.d("main", personId)
+        }
+
+    }
+
+    private fun getHashKey() {
+        try {
+            val info = packageManager.getPackageInfo(
+                "com.example.loginsocialnetwork",
+                PackageManager.GET_SIGNATURES)
+            for (signature in info.signatures) {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.e("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            }
+        } catch (e: PackageManager.NameNotFoundException) {
+
+        } catch (e: NoSuchAlgorithmException) {
+
+        }
     }
 
 }
